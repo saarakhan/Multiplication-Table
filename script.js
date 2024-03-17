bd41c03d43edf77222b090bfb5de837c763ee324
Saara Khan <khansarah0716@gmail.com>
2024-03-17 17:34:06 +0530

    Adding all files

---
+++ script.js
@@ -0,0 +1,19 @@
+const tableForm = document.querySelector(".table-from");
+const getValue = document.getElementById("amount");
+
+tableForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const num = parseInt(getValue.value);
+
+    if (isNaN(num)) {
+        alert("Please enter input first. Thanks");
+    } else if (num < 1) {
+        alert("Less then or equals 0 numbers can't calculate.");
+    } else {
+        for (let i = 1; i <= 10; i++) {
+            document.querySelector(".append-result").innerHTML +=
+                "<li>" + num + " x " + i + " = " + num * i + "<li/>";
+        }
+    }
+});
